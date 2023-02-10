0b50e30cb4a9e26729c7f6f551e5a0edf762b8c6
Admon1123 <oattaku117@gmail.com>
2023-02-10 23:15:39 +0700

    add some tag

---
+++ app.js
@@ -0,0 +1,5 @@
+let h1 = document.getElementById("demo").innerHTML = "Hello Github";
+
+let p1 = document.getElementById("p_demo").innerHTML = "This is github";
+
+let p2 = document.getElementById("p_demo2").innerHTML = "This is github this is version control";
